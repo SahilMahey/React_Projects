084dd850d63aed9c3cb5b7e508257aaf43aa242c
Sahil <90522898+SahilMahey@users.noreply.github.com>
2023-06-23 18:50:43 +0000

    Update Main_Header.css and Main_Header.js

--- space_project/src/Components/Main_Header/Main_Header/Main_Header.js
+++ space_project/src/Components/Main_Header/Main_Header/Main_Header.js
@@ -6,26 +6,39 @@ import hamburger_icon from './icon-hamburger.svg'
 import close_icon from './icon-close.svg'
 import './Main_Header.css'
 function Main_Header() {
-  const [hamburger, sethamburger] = useState(true);
+  const [hamburger, sethamburger] = useState(false);
+  
   function handle_hamburger()
   {
+    if (!hamburger)
+    {
+      document.getElementsByClassName('container3')[0].style.display = 'block'
+    }
+    else if (hamburger)
+    {
+       document.getElementsByClassName('container3')[0].style.display = 'none'
+    }
     sethamburger(!hamburger)
- 
+    
+    
   }
   return (
     <>
+    <div className='main_header_container'>
     <div className="box">
     <Link  to = "/" className="container1"><img src={Logo} alt=""/></Link>
     <button onClick={handle_hamburger}><img src={!hamburger ? hamburger_icon : close_icon} alt="" /></button>
-     {hamburger && <div className="container3">
-    <ul className="navbar">
-      <NavLink to="/">00  Home</NavLink>
-      <NavLink to="Destination">01  Destination</NavLink>
-      <NavLink to="Crew">02  Crew</NavLink>
-      <NavLink to="Technology">03  Technology</NavLink>
+    <div className="container3">
+  <ul className="navbar">
+      <NavLink to="/">00 Home</NavLink>
+      <NavLink to="Destination">01 Destination</NavLink>
+      <NavLink to="Crew">02 Crew</NavLink>
+      <NavLink to="Technology">03 Technology</NavLink>
+  
     </ul>
-    </div> }
+    </div> 
     <div className="line"></div> 
+  </div>
   </div>
   <Outlet />
   </>
