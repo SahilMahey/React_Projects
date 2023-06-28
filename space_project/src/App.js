7e4c305c4e25d6b9a2a86c96a04d41705dc9283b
SahilMahey <smahey@mun.ca>
2023-06-27 21:50:07 -0230

    b

--- space_project/src/App.js
+++ space_project/src/App.js
@@ -15,26 +15,21 @@ function App() {
   return (
     
     <BrowserRouter>
-     
+     <MainHeader />
     <Routes>
-    <Route path = "/" element = {<MainHeader />}>
-      <Route path="/" element={<Home />} />
-      <Route path="Destination" element={<Destination />}>
-      <Route index element={<ContentDest title="Moon" />} />
- 
-      <Route path="Mars" element={<ContentDest title = "Mars" />} />
-      <Route path="Europa" element={<ContentDest title = "Europa"/>} />
-      <Route path="Titan" element={<ContentDest title = "Titan" />} />
-      </Route>
+      <Route  path ="/" element={ <Home/>}/>
+      <Route path="Destination" element={<Destination />} >
+        <Route index />
+        <Route path= "Mars"/>
+        <Route path= "Europa" />
+        <Route path= "Titan" />
+        </Route>
       <Route path="Crew" element={<Crew />} />
       <Route path="Technology" element={<Technology />} />
-    </Route>
-    
     
     <Route path = "*" element ={<Error />} />
     </Routes>
-
-    </BrowserRouter>
+</BrowserRouter>
   );
 }
 
--- space_project/src/Components/Destination/Destination.js
+++ space_project/src/Components/Destination/Destination.js
@@ -2,33 +2,46 @@ import React, { useState} from 'react'
 import array from '../../data.json'
 import ContentDest from './Content_dest'
 import './Destination.css'
-import { NavLink, Outlet} from 'react-router-dom'
+import { NavLink, Outlet, Link} from 'react-router-dom'
 
 function Destination() {
   
+  const [data,setdata] = useState(array.destinations[0]);
+  function filter_data(title)
+  {
+   
+    let maker = array.destinations.filter((element)=>
+    {
+  
+        return element.name === title
+    })
+    console.log(maker[0])
+    setdata(maker[0])
+  }
+ 
 
   return (
 
     <div className="destination_container">
       <h3>01 Pick your destination</h3>
       <div className="destination_content">
-      <img src = "" alt = "" />
+      <img src={require("./image-moon.png").default}  alt = "" />
     <ul>
     <li>
-            <NavLink to="/Destination" end >Moon</NavLink >
-          </li>
-          <li>
-            <NavLink to="Mars" >Mars</NavLink>
-          </li>
-          <li>
-            <NavLink to="Europa" >Europa</NavLink>
-          </li>
-          <li>
-            <NavLink to="Titan">Titan</NavLink>
-          </li>
+    <NavLink  to="/Destination" onClick = {()=>filter_data("Moon")} end>Moon</NavLink >
+    </li>
+    <li>
+      <NavLink  to="Mars" onClick = {()=>filter_data("Mars")}>Mars</NavLink>
+    </li>
+    <li>
+      <NavLink  to="Europa" onClick = {()=>filter_data("Europa")}>Europa</NavLink>
+    </li>
+    <li>
+      <NavLink  to="Titan" onClick = {()=>filter_data("Titan")}>Titan</NavLink>
+    </li>
     </ul>
   </div>
- < Outlet/>
+  <ContentDest object = {data} />
  </div>
   )
 }
