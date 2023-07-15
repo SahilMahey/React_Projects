f068c2c5a0759bd9543d74cacaf7be723f5bb86a
SahilMahey <smahey@mun.ca>
2023-07-15 12:02:40 -0230

    k

--- find_recipie/src/index.js
+++ find_recipie/src/index.js
@@ -1,17 +1,15 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-
-import App from "./App";
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './App';
 
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
+ReactDOM.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
